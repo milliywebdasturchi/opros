9c2628f0f58685f88e615082fec325a3d6328463
milliywebdasturchi <milliywebdasturchi@mail.ru>
2022-03-05 07:31:53 +0500

    update file

--- admin/check.php
+++ admin/check.php
@@ -8,23 +8,24 @@ if(isset($_POST['send'])) {
     $sql_2 = mysqli_query($conn, "SELECT * FROM javoblar WHERE sorov_id = '$sorov_id'");
 
     
-        $sum = 0;
+    $sum = 0;
+    $arr = array();
+    while ($row_2 = mysqli_fetch_array($sql_2)) {
         while($row_1 = mysqli_fetch_array($sql_1)) {
             $res_1 = explode(' / ', $row_1['answers']);
             $num = count($res_1);
 
-            while ($row_2 = mysqli_fetch_array($sql_2)) {
-            
-                for($i = 0; $i < $num; $i++) {
-                    if($res_1[$i] == $row_2['javob']) {
-                        $sum++;
-                        echo $res_1[$i] . " - " . $row_2['javob'] . " - " .$sum . "<br>";
-                        // exit;
-                        mysqli_query($conn, "UPDATE `javoblar` SET `count` = `count` + 1 WHERE id = '$row_2[id]'");
-                    }
-                    
+            for($i = 0; $i < $num; $i++) {
+                if($res_1[$i] == $row_2['javob']) {
+                    $sum++;
                 }
-            }          
-        }  
+            }
+            
+        }
+        array_push($arr, $sum);
+        $sum = 0;          
+    } 
+    echo "<pre>";
+    print_r($arr);
    
 }
--- sendform.php
+++ sendform.php
@@ -4,29 +4,53 @@ include "./db.php";
 
 if (isset($_POST['send'])) {
 
+	// echo "<pre>";
+	// print_r($_POST);
+	// echo "</pre>";
+	// die;
+
 	$opros = $_POST['opros'];
 
 	if($opros === 'anonym') {
 
-		$sorov_id = $_POST['sorov_id'];
-		$viloyat_name = $_POST['viloyat_name'];
+		$sorov_id = mysqli_real_escape_string($conn, $_POST['sorov_id']);
+		$viloyat_name = mysqli_real_escape_string($conn, $_POST['viloyat_name']);
 		$fullname = null;
 		$phone = null;
 		$address = null;
 
 		// select savol id
 		$savol_query_id = mysqli_query($conn, "SELECT id FROM savollar WHERE s_id = '$sorov_id'");
-		$arr_jvb = array();
+		$arr = array();
 		while ($row_id = mysqli_fetch_assoc($savol_query_id)) {
 		 	// savol id 
 		 	$r_id = $row_id['id'];
-			$jvb.$r_id = $_POST['jvb_'.$r_id]." / ";
+			$jvb = $_POST['jvb_'.$r_id];
 			
-			$jvb = explode(' / ', $jvb.$r_id);
-			// $arr_jvb = array_merge($arr_jvb, $jvb);
-			$arr_jvb .= trim(implode(' / ', $jvb), 'Array');
+			array_push($arr, $jvb);
+			
 		}
-		$jvb_all = trim($arr_jvb, 'Array  / ');
+
+		$ans_1 = mysqli_real_escape_string($conn, $arr[0]);
+		$ans_2 = mysqli_real_escape_string($conn, $arr[1]);
+		$ans_3 = mysqli_real_escape_string($conn, $arr[2]);
+		$ans_4 = mysqli_real_escape_string($conn, $arr[3]);
+		$ans_5 = mysqli_real_escape_string($conn, $arr[4]);
+		$ans_6 = mysqli_real_escape_string($conn, $arr[5]);
+		$ans_7 = mysqli_real_escape_string($conn, $arr[6]);
+		$ans_8 = mysqli_real_escape_string($conn, $arr[7]);
+		$ans_9 = mysqli_real_escape_string($conn, $arr[8]);
+		$ans_10 = mysqli_real_escape_string($conn, $arr[9]);
+		$ans_11 = mysqli_real_escape_string($conn, $arr[10]);
+		$ans_12 = mysqli_real_escape_string($conn, $arr[11]);
+		$ans_13 = mysqli_real_escape_string($conn, $arr[12]);
+		$ans_14 = mysqli_real_escape_string($conn, $arr[13]);
+		$ans_15 = mysqli_real_escape_string($conn, $arr[14]);
+		$ans_16 = mysqli_real_escape_string($conn, $arr[15]);
+		$ans_17 = mysqli_real_escape_string($conn, $arr[16]);
+		$ans_18 = mysqli_real_escape_string($conn, $arr[17]);
+		$ans_19 = mysqli_real_escape_string($conn, $arr[18]);
+		$ans_20 = mysqli_real_escape_string($conn, $arr[19]);
 
 		$subject = htmlspecialchars(htmlentities($_POST['subject']));
 
@@ -35,7 +59,8 @@ if (isset($_POST['send'])) {
 		date_default_timezone_set('Asia/Tashkent');
 		$createDate = time();
 
-		$insert = mysqli_query($conn, "INSERT INTO hisobot(`ip`,`v_name`,`s_id`,`fullName`,`phoneNumber`,`address`,`answers`,`subject`,`createDate`) VALUES ('$myip','$viloyat_name','$sorov_id',null,null,null,'$jvb_all','$subject','$createDate')");
+		$insert = mysqli_query($conn, "INSERT INTO hisobot(`ip`,`v_name`,`s_id`,`fullName`,`phoneNumber`,`address`,`ans_1`,`ans_2`,`ans_3`,`ans_4`,`ans_5`,`ans_6`,`ans_7`,`ans_8`,`ans_9`,`ans_10`,`ans_11`,`ans_12`,`ans_13`,`ans_14`,`ans_15`,`ans_16`,`ans_17`,`ans_18`,`ans_19`,`ans_20`,`subject`,`createDate`) VALUES ('$myip','$viloyat_name','$sorov_id',null,null,null,'$ans_1','$ans_2','$ans_3','$ans_4','$ans_5','$ans_6','$ans_7','$ans_8','$ans_9','$ans_10','$ans_11','$ans_12','$ans_13','$ans_14','$ans_15','$ans_16','$ans_17','$ans_18','$ans_19','$ans_20','$subject','$createDate')");
+		
 		if($insert) {
 			header("Location: index.php?msg=success");
 			exit;
@@ -46,7 +71,7 @@ if (isset($_POST['send'])) {
 
 	} else if($opros === 'verify') {
 
-		$sorov_id = $_POST['sorov$sorov_id'];
+		$sorov_id = $_POST['sorov_id'];
 		$viloyat_name = $_POST['viloyat_name'];
 		$fullname = $_POST['fullname'];
 		$phone = $_POST['phone'];
@@ -54,17 +79,36 @@ if (isset($_POST['send'])) {
 
 		// select savol id
 		$savol_query_id = mysqli_query($conn, "SELECT id FROM savollar WHERE s_id = '$sorov_id'");
-		$arr_jvb = array();
+		$arr = array();
 		while ($row_id = mysqli_fetch_assoc($savol_query_id)) {
 		 	// savol id 
 		 	$r_id = $row_id['id'];
-			$jvb.$r_id = $_POST['jvb_'.$r_id]." / ";
+			$jvb = $_POST['jvb_'.$r_id];
 			
-			$jvb = explode(' / ', $jvb.$r_id);
-			// $arr_jvb = array_merge($arr_jvb, $jvb);
-			$arr_jvb .= trim(implode(' / ', $jvb), 'Array');
+			array_push($arr, $jvb);
+			
 		}
-		$jvb_all = trim($arr_jvb, 'Array  / ');
+		
+		$ans_1 = mysqli_real_escape_string($conn, $arr[0]);
+		$ans_2 = mysqli_real_escape_string($conn, $arr[1]);
+		$ans_3 = mysqli_real_escape_string($conn, $arr[2]);
+		$ans_4 = mysqli_real_escape_string($conn, $arr[3]);
+		$ans_5 = mysqli_real_escape_string($conn, $arr[4]);
+		$ans_6 = mysqli_real_escape_string($conn, $arr[5]);
+		$ans_7 = mysqli_real_escape_string($conn, $arr[6]);
+		$ans_8 = mysqli_real_escape_string($conn, $arr[7]);
+		$ans_9 = mysqli_real_escape_string($conn, $arr[8]);
+		$ans_10 = mysqli_real_escape_string($conn, $arr[9]);
+		$ans_11 = mysqli_real_escape_string($conn, $arr[10]);
+		$ans_12 = mysqli_real_escape_string($conn, $arr[11]);
+		$ans_13 = mysqli_real_escape_string($conn, $arr[12]);
+		$ans_14 = mysqli_real_escape_string($conn, $arr[13]);
+		$ans_15 = mysqli_real_escape_string($conn, $arr[14]);
+		$ans_16 = mysqli_real_escape_string($conn, $arr[15]);
+		$ans_17 = mysqli_real_escape_string($conn, $arr[16]);
+		$ans_18 = mysqli_real_escape_string($conn, $arr[17]);
+		$ans_19 = mysqli_real_escape_string($conn, $arr[18]);
+		$ans_20 = mysqli_real_escape_string($conn, $arr[19]);
 
 		$subject = htmlspecialchars(htmlentities($_POST['subject']));
 
@@ -73,7 +117,8 @@ if (isset($_POST['send'])) {
 		date_default_timezone_set('Asia/Tashkent');
 		$createDate = time();
 
-		$insert = mysqli_query($conn, "INSERT INTO hisobot(`ip`,`v_name`,`s_id`,`fullName`,`phoneNumber`,`address`,`answers`,`subject`,`createDate`) VALUES ('$myip','$viloyat_name','$sorov_id','$fullname','$phone','$address','$jvb_all','$subject','$createDate')");
+		$insert = mysqli_query($conn, "INSERT INTO hisobot(`ip`,`v_name`,`s_id`,`fullName`,`phoneNumber`,`address`,`ans_1`,`ans_2`,`ans_3`,`ans_4`,`ans_5`,`ans_6`,`ans_7`,`ans_8`,`ans_9`,`ans_10`,`ans_11`,`ans_12`,`ans_13`,`ans_14`,`ans_15`,`ans_16`,`ans_17`,`ans_18`,`ans_19`,`ans_20`,`subject`,`createDate`) VALUES ('$myip','$viloyat_name','$sorov_id',null,null,null,'$ans_1','$ans_2','$ans_3','$ans_4','$ans_5','$ans_6','$ans_7','$ans_8','$ans_9','$ans_10','$ans_11','$ans_12','$ans_13','$ans_14','$ans_15','$ans_16','$ans_17','$ans_18','$ans_19','$ans_20','$subject','$createDate')");
+		
 		if($insert) {
 			header("Location: index.php?msg=success");
 			exit;
