76bb7985715cda67d445df64d453983f7568292d
milliywebdasturchi <milliywebdasturchi@mail.ru>
2022-03-07 14:30:23 +0500

    add new file

--- db/survey.sql
+++ db/survey.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 07 2022 г., 10:52
+-- Время создания: Мар 07 2022 г., 12:30
 -- Версия сервера: 5.6.51
 -- Версия PHP: 7.4.27
 
@@ -38,6 +38,20 @@ CREATE TABLE `answers` (
   `percent` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `answers`
+--
+
+INSERT INTO `answers` (`id`, `s_is`, `q_id`, `ans_uz`, `ans_ru`, `ans_en`, `count`, `percent`) VALUES
+(1, 1, 1, 'Ha, mavjud', 'Да, имеются', 'Yes, there are', 0, 0),
+(2, 1, 1, 'Yo‘q, mavjud emas', 'Нет, не имеются', 'No, not available', 0, 0),
+(3, 1, 1, 'Javob berishga qiynalaman', 'Затрудняюсь ответить', 'I am at a loss to answer', 0, 0),
+(4, 1, 2, 'Bunday holatlar mavjud emas', 'Таких фактов не существуют', 'Such facts do not exist', 0, 0),
+(5, 1, 2, 'Yuqori darajada', 'На высоком уровне', 'At a high level', 0, 0),
+(6, 1, 2, 'Past darajada', 'На низком уровне', 'At a low level', 0, 0),
+(7, 1, 3, 'Ha', 'Да', 'Yes', 0, 0),
+(8, 1, 3, 'Yo‘q', 'Нет', 'No', 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +65,19 @@ CREATE TABLE `questions` (
   `q_ru` varchar(255) NOT NULL,
   `q_en` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `questions`
+--
+
+INSERT INTO `questions` (`id`, `s_id`, `q_uz`, `q_ru`, `q_en`) VALUES
+(1, 1, 'Sizning fikringizcha faoliyat yuritayotgan xududingizda Farmasevtika tarmog‘ini rivojlantirish agentligi va uning hududiy bo‘linmalari faoliyatida korrupsion holatlar mavjudmi?', 'Как вы считаете, имеются ли коррупционные факты в деятельности Агентства по развитию фармацевтической отрасли и его региональных подразделений в Вашем регионе?', 'In your opinion, are there any facts of corruption in the activities of the Agency for the Development of the Pharmaceutical Industry and its regional divisions in your region?'),
+(2, 1, 'Hududingizda Farmasevtika tarmog‘i bo‘linmasidagi korrupsiya darajasini qanday baholagan bo‘lardingiz?', 'Как вы оценили бы уровень коррупционности подразделения Фармацевтического агентства в Вашем регионе?', 'How would you rate the level of corruption of the Pharmaceutical Agency unit in your region?'),
+(3, 1, 'Farmasevtika tarmog‘i faoliyatida shaxsan o‘zingiz korrupsiya holatlariga duch kelganmisiz?', 'Сталкивались ли Вы лично с коррупционными фактами в деятельности Фармацевтической отрасли?', 'Have you personally encountered corruption in the pharmaceutical industry?'),
+(4, 1, 'Farmasevtika tarmog‘i faoliyatida korrupsion holatning qanday ko‘rinishiga duch kelgansiz?', 'С каким видом коррупционного проявления в Фармацевтической отрасли Вы сталкивались?', 'What type of corruption have you encountered in the pharmaceutical industry?'),
+(5, 1, 'Farmasevtika tarmog‘i faoliyatida qaysi sohalarda korrupsion holatlar ko‘proq mavjud deb hisoblaysiz?', 'По Вашему мнению в каких сферах Фармацевтической отрасли высокий уровень коррупционных фактов?', 'In your opinion, in what areas of the pharmaceutical industry is there a high level of corruption facts?'),
+(6, 1, 'Farmasevtika tarmog‘i faoliyatida so‘nggi 3 yilda korrupsiya darajasining o‘zgarishini qanday baholaysiz?', 'Как Вы оцениваете изменения уровня коррупции в Фармацевтической отрасли за последние 3 года?', 'How do you assess the changes in the level of corruption in the pharmaceutical industry over the past 3 years?'),
+(7, 1, 'Farmasevtika tarmog‘i faoliyatida korrupsiya faktlari yuzasidan habar berish uchun @Stop_corruptionbot telegram bot mavjudligini bilasizmi?', 'Осведомлены ли Вы о наличии телеграм бота @Stop_corruptionbot, позволяющего отправлять сведения о коррупционных фактах?', 'Are you aware of the presence of the @Stop_corruptionbot telegram bot, which allows you to send information about corruption facts?');
 
 -- --------------------------------------------------------
 
@@ -187,13 +214,13 @@ ALTER TABLE `surveys`
 -- AUTO_INCREMENT для таблицы `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `regions`
