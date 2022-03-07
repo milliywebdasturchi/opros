947b478e7bf444cf1d3484a2257958f485443092
milliywebdasturchi <milliywebdasturchi@mail.ru>
2022-03-07 09:26:25 +0500

    last commit

--- db.php
+++ db.php
@@ -5,6 +5,8 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "opros";
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 if(!$conn) {
